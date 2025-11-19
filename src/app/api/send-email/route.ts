19b018cef081368b05af1ea1acc7a536b3ada199
rai-kriti <raikriti628@gmail.com>
2025-11-19 19:51:06 +0530

    Disable ESLint during Vercel build

--- src/app/api/send-email/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  // redirect to your latest Google Drive resume
-  return NextResponse.redirect("https://drive.google.com/file/d/16JRbtJjmHAXkAghOijU3YPUWby_Tn9Og/view?usp=sharing");
-}
